4589618844fa0dd657566a6fc73cd764fa9a66c1
Dialla23 <diallomahamoudou39@yahoo.fr>
2023-06-01 00:21:57 -0300

    Password added

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 // Assignment code here
-
+function generatePassword() {
+  var length = 8,
+  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+  retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+  }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#password");
+if (window.confirm("Do you really want to leave?")) {
+  window.open("exit.html", "Thanks for Visiting!");
+}g
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(password);
   var passwordText = document.querySelector("#password");
-
+  console.log();
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
+let sign = prompt("password");
+
+if (sign.toLowerCase() === "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
 generateBtn.addEventListener("click", writePassword);
